260b10bd28af547d7f41efefac1d78f0fc229ab5
Optimizacija Sajta SEO <banjavrujci@gmail.com>
2026-07-20 17:52:31 +0200

    5

---
+++ push_gallery.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d "C:\Users\banja\Desktop\AI\CLAUDE\banja-vrujci"
+git add index.html kategorija.html smestaj.html stranica.html
+git commit -m "Add gallery_images display + weather widget on all 4 templates"
+git push origin main
+pause
